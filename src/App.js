7b5c50c56c35c0bcd8062ba81dfc479c6699fb3a
iftekharanwar <iftekhar@example.com>
2024-05-10 20:39:37 +0000

    Integrate RatingForm component into App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import ProfessorList from './components/ProfessorList';
+import RatingForm from './components/RatingForm'; // Importing the RatingForm component
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ProfessorList />
+      <RatingForm /> {/* Adding the RatingForm component to the page */}
       {/* Rest of the app components will go here */}
     </div>
   );
