2708181edf1902304d4592992db98591847953ea
iftekharanwar <iftekhar@example.com>
2024-05-12 04:00:51 +0000

    Handle undefined properties in Professor component

--- src/components/Professor.js
+++ src/components/Professor.js
@@ -2,26 +2,31 @@ import React from 'react';
 import { Box, Flex, Text, Button } from '@chakra-ui/react';
 
 const Professor = ({ details }) => {
+  // Check if details and details.ratings are defined
+  const ratings = details && details.ratings ? details.ratings : { clarity: 'N/A', helpfulness: 'N/A', easiness: 'N/A' };
+  const comments = details && details.comments ? details.comments : ['No comments available'];
+  const tags = details && details.tags ? details.tags : ['No tags available'];
+
   return (
     <Box p={5} shadow="md" borderWidth="1px">
       <Flex direction="column" align="start">
-        <Text fontSize="2xl" fontWeight="bold">{details.name}</Text>
-        <Text fontSize="l">{details.department}, {details.university}</Text>
+        <Text fontSize="2xl" fontWeight="bold">{details ? details.name : 'Professor Name'}</Text>
+        <Text fontSize="l">{details ? `${details.department}, ${details.university}` : 'Department, University'}</Text>
         <Box>
           {/* Ratings would be displayed here */}
-          <Text fontSize="m">Clarity: {details.ratings.clarity}</Text>
-          <Text fontSize="m">Helpfulness: {details.ratings.helpfulness}</Text>
-          <Text fontSize="m">Easiness: {details.ratings.easiness}</Text>
+          <Text fontSize="m">Clarity: {ratings.clarity}</Text>
+          <Text fontSize="m">Helpfulness: {ratings.helpfulness}</Text>
+          <Text fontSize="m">Easiness: {ratings.easiness}</Text>
         </Box>
         <Box>
           {/* Comments would be displayed here */}
-          {details.comments.map((comment, index) => (
+          {comments.map((comment, index) => (
             <Text key={index} fontSize="sm">{comment}</Text>
           ))}
         </Box>
         <Flex wrap="wrap">
           {/* Tags would be displayed here */}
-          {details.tags.map((tag, index) => (
+          {tags.map((tag, index) => (
             <Button key={index} size="sm" m={1}>{tag}</Button>
           ))}
         </Flex>
